a3a233a49e9d19c2c8fe7a9d57da65cb85fc004e
David H.O <cuentaporsi2@gmail.com>
2024-08-08 18:17:52 -0500

    a

--- gradio_app/app.py
+++ gradio_app/app.py
@@ -1,17 +1,23 @@
+import streamlit as st
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+from util import set_background
+
+
 import numpy as np
 import joblib
-import gradio as gr
+from utils import haller_index_
 
+
+
+# Function to test model
 def test_model(pectus_tomografía):
     indx = haller_index_(pectus_tomografía)
     model = joblib.load('model.pkl')
-    # Ensure prediction is a numpy array
     prediction = np.array(model.predict(indx.reshape(-1, 1)))
-    
-    # Perform the addition
     prediction_adjusted = np.round(prediction, 2) + 2
-    
-    # Ensure indx is processed similarly
     indx_adjusted = np.round(indx, 2) + .5
     recomendaciones = {
         'Normal': 'No se requiere ninguna acción específica. Se recomiendan revisiones periódicas',
@@ -19,11 +25,8 @@ def test_model(pectus_tomografía):
         'Moderado': 'Se recomienda consultar a un especialista sobre posibles opciones de tratamiento',
         'Severo': 'Se recomienda consultar inmediatamente a un especialista. Discutir opciones de tratamiento quirúrgico y no quirúrgico.'
     }
-
-    # Adjusted to handle scalar value from numpy array
-    indx_scalar = indx_adjusted.item()  # Convert numpy array to scalar
-    prediction_scalar = prediction_adjusted.item()  # Convert numpy array to scalar
-
+    indx_scalar = indx_adjusted.item()
+    prediction_scalar = prediction_adjusted.item()
     if indx_scalar < 2.5:
         indxt = 'Normal'
     elif indx_scalar >= 2.5 and indx_scalar < 3.25:
@@ -32,7 +35,6 @@ def test_model(pectus_tomografía):
         indxt = 'Moderado'
     else:
         indxt = 'Severo'
-
     if prediction_scalar < 2.5:
         predictiont = 'Normal'
     elif prediction_scalar >= 2.5 and prediction_scalar < 3.25:
@@ -41,6 +43,18 @@ def test_model(pectus_tomografía):
         predictiont = 'Moderado'
     else:
         predictiont = 'Severo'
-
     return "Diagnóstico: \nÍndice de Haller Actual: {} ({})\n\nÍndice de Haller Post Operatorio: {} ({})\n\nRecomendaciones: {}".format(prediction_scalar, predictiont, indx_scalar, indxt, recomendaciones[predictiont])
 
+# Set background
+set_background('PExpert.png')
+
+# Streamlit app
+st.title("PExpert")
+st.header("Precision and confidence in your pectus surgery")
+
+uploaded_file = st.file_uploader("Please upload your chest CT image in .png or .jpg", type=["png", "jpg"])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert('RGB')
+    st.image(image, use_column_width=True)
+    diagnosis = test_model(image)
+    st.write(diagnosis)
---
+++ gradio_app/util.py
@@ -0,0 +1,7 @@
+import base64
+
+import streamlit as st
+from PIL import ImageOps, Image
+import numpy as np
+
+
